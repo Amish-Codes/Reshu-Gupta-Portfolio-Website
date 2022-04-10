aa381470a44016bcbd7aad5c25964197ffdd6cc2
Amish Gupta <62796030+amish1498@users.noreply.github.com>
2022-04-10 13:25:06 +0530

    flip image

---
+++ flip.js
@@ -0,0 +1,18 @@
+// on click class heads change image
+let count = 0;
+function clickImage() {
+    // change src of image
+    var image = document.getElementsByClassName("heads");
+
+    if (count%2 == 0) {
+        image[0].style.transition = "transform 1s";
+        image[0].src = "images/Reshu Image 2.jpeg";
+        image[0].style.transform = "rotateY(-180deg)";
+    }
+    else {
+        image[0].style.transform = "rotateY(0deg)";
+        image[0].style.transition = "transform 1s";
+        image[0].src = "images/Reshu rp.jpeg";
+    }
+    count++;
+}
